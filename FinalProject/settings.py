a0fa2894e83cc880ef8d341d7421d2cebe8a8137
vinso <8vinso8@gmail.com>
2022-05-05 17:32:37 +1000

    Django init

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -37,18 +37,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'mainAPI',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+# Доступ в API отовсюду
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'FinalProject.urls'
 
