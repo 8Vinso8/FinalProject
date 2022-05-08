937392efa60ecd464229fe5ce587cbb9e79e67e0
Vinso <8vinso8@gmail.com>
2022-05-08 10:24:10 +1000

    Django api is now available at /api

--- django-backend/FinalProject/settings.py
+++ django-backend/FinalProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j9bhx@e-r56a^$^_i6e$)a5bbhz2a^%5&9_t&pip&k2!lup%58
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- django-backend/mainAPI/views.py
+++ django-backend/mainAPI/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 
 # redis connection
 # redis = rd.Redis(host=’redis’, port=6379, db=0)
+
+def test(request):
+    return HttpResponse("This is Django")
