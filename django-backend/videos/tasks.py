e063136761a35de6d099cd9b86396ebc9d773e95
V0D14ka <minecraft35510880@gmail.com>
2022-07-01 01:43:18 +1000

    Fixed mailing

--- django-backend/videos/tasks.py
+++ django-backend/videos/tasks.py
@@ -5,9 +5,12 @@ from FinalProject.settings import EMAIL_HOST_USER
 
 
 @app.task
-def send_spam(owner, mesg):
+def send_spam(owner, title):
     subs = CustomUser.objects.get(id=owner).subscribers.all()
+    name = CustomUser.objects.get(id=owner).username
+    mesg = 'Здравствуйте. У пользователя ' + name + ' вышло новое видео - ' \
+           + '"' + title + '"' + '. С уважением, администрация SeaVid!'
     for i in subs:
         uuser = CustomUser.objects.get(id=i.user_id).email
-        send_mail('Оповещение', mesg, EMAIL_HOST_USER, [uuser])
+        send_mail('Новое видео', mesg, EMAIL_HOST_USER, [uuser])
 
