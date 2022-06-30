aa77027a293f406d08e2dc67313b62f0d6fac853
V0D14ka <minecraft35510880@gmail.com>
2022-06-30 22:33:55 +1000

    Fixed secret leak.

--- django-backend/FinalProject/settings.py
+++ django-backend/FinalProject/settings.py
@@ -1,5 +1,9 @@
 import os
 from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+import dotenv
+
+load_dotenv(find_dotenv())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j9bhx@e-r56a^$^_i6e$)a5bbhz2a^%5&9_t&pip&k2!lup%58'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,8 +145,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'lolbutt.noreply@gmail.com'
-EMAIL_HOST_PASSWORD = 'xyixnibhfapnvinx'
+EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
 
 ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_UNIQUE_EMAIL = True
