564da6100935457770bb35c4326ffbc4430d6523
Vring <44764023+Vring@users.noreply.github.com>
2022-07-01 10:53:38 +1000

    Update settings.py

--- django-backend/FinalProject/settings.py
+++ django-backend/FinalProject/settings.py
@@ -16,9 +16,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['localhost', '.myhappypage.ninja']
+CSRF_TRUSTED_ORIGINS = ['http://localhost', 'https://*.myhappypage.ninja']
 
 
 # Application definition
