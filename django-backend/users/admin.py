1ded370ecf9328a6502717d2930a75e792595fad
Olimp666 <bolotaev.rala@students.dvfu.ru>
2022-07-02 17:15:22 +1000

    backend fix

--- django-backend/users/admin.py
+++ django-backend/users/admin.py
@@ -6,7 +6,6 @@ from users.models import PostSend
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    exclude = ['last_name', 'first_name', ]
     list_display = ['email', 'username', ]
 
 
