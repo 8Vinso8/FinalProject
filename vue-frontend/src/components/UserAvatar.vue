a6768af1e93a790dbe7b58790c43c9eefc1d5d84
Olimp666 <bolotaev.rala@students.dvfu.ru>
2022-07-06 08:36:32 +1000

    Add components

---
+++ vue-frontend/src/components/UserAvatar.vue
@@ -0,0 +1,30 @@
+<template>
+    <img :src="avatar" />
+</template>
+
+<script>
+
+const axios = require('axios');
+export default {
+    name: "UserAvatar",
+    props: {
+        user_id: Number
+    },
+    async created() {
+        this.avatar = (await this.GetUserAvatar(this.$props.user_id)).data.avatar
+    },
+    data() {
+        var avatar
+        return {
+            avatar
+        }
+    },
+    methods: {
+        async GetUserAvatar(id) {
+            const formData = new FormData();
+            const headers = { 'Content-Type': 'application/json' };
+            return axios.get(this.backhost + '/api/users/' + id, formData, { headers });
+        }
+    }
+};
+</script>
---
+++ vue-frontend/src/components/VideoList.vue
@@ -0,0 +1,58 @@
+<template>
+    <div v-for="video in videos" :key="video.id"
+        class="list-group-item list-group-item-action list-group-item-secondary item">
+        <router-link :to="'/watch/' + video.id">
+            <img :src="video.thumbnail" class="img-thumbnail float-left" />
+            <Avatar :user_id="video.user_id" class="img-thumbnail float-left" />
+            <span style=" font-size: 2em; position: flex; justify-content:center !important;">{{ video.title }}</span>
+            <span style=" font-size: 2em; position: flex; justify-content:center !important;">{{ video.user }}</span>
+            <i class="bi bi-hand-thumbs-up-fill fa-3x" style="font-size: 150%;"></i>
+            <span style=" font-size: 2em; position: flex; justify-content:center !important;">{{ video.likes_count
+            }}</span>
+        </router-link>
+    </div>
+</template>
+
+<script>
+
+import Avatar from './UserAvatar.vue'
+export default {
+    name: "VideoList",
+    props: {
+        videos: Array
+    },
+    components: { Avatar }
+};
+</script>
+
+<style scoped>
+h3 {
+    margin: 40px 0 0;
+}
+
+ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+li {
+    display: inline-block;
+    margin: 0 10px;
+}
+
+a {
+    color: #42b983;
+}
+
+img {
+    height: 120px !important;
+    width: auto;
+}
+
+;
+
+.item {
+    width: 200px;
+    height: 120px;
+}
+</style>
