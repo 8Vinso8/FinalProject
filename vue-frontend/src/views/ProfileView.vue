d46ca4c3cd1686fb52df84f54816e7662c4d663e
Daniil Isakov <12859907+oplexz@users.noreply.github.com>
2022-07-09 04:05:34 +0300

    visual: basic css for ProfileView

--- vue-frontend/src/views/ProfileView.vue
+++ vue-frontend/src/views/ProfileView.vue
@@ -1,38 +1,39 @@
 <template>
-    <avatar :user_id="me.id" class="comment-user-avatar rounded-circle border border-dark" />
-    <button class="btn btn-danger" @click="Logout()">Logout</button>
+    <div class="container d-flex flex-column align-items-center gap-3">
+        <avatar :user_id="me.id" class="comment-user-avatar rounded-circle border border-dark" />
+        <button class="btn btn-danger" @click="Logout()">Logout</button>
+    </div>
 </template>
 
 <script>
-
 import Avatar from "@/components/UserAvatar.vue";
 const axios = require("axios");
 export default {
     name: "ProfileView",
     async created() {
         this.authkey = this.$cookies.get("authkey");
-        this.me = await this.GetMe()
+        this.me = await this.GetMe();
     },
     data() {
-        var authkey, me
+        var authkey, me;
         return {
-            authkey, me
-        }
+            authkey,
+            me
+        };
     },
     components: { Avatar },
     methods: {
         Logout() {
             const headers = { "Content-Type": "application.json", Authorization: `Token ${this.authkey}` };
             axios.post(this.backhost + "/api/users/logout", { headers });
-            this.$cookies.set("authkey", 'cookie', "30d");
-            this.$router.push('/')
+            this.$cookies.set("authkey", "cookie", "30d");
+            this.$router.push("/");
         },
         async GetMe() {
             const headers = { "Content-Type": "application.json", Authorization: `Token ${this.authkey}` };
             return (await axios.get(this.backhost + "/api/users/current", { headers })).data;
         }
     }
-
 };
 </script>
 <style scoped>
